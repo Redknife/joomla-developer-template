cf439847cfcb7001927613a2125762a9f95cb2b4
Sergey Rogachev <realredknife@gmail.com>
2016-11-02 14:29:38 +0500

    Added spoilers

--- src/js/plugins/spoiler.js
+++ src/js/plugins/spoiler.js
@@ -1,33 +1,90 @@
+/* eslint new-cap: 0 */
+/* eslint no-unused-expressions: 0 */
 import $ from 'jquery';
 
-export default (selector) => {
+const defaultSettings = {
+  togglerSfx: '_toggle',
+  controlSfx: '_control',
+  boxSfx: '_box',
+  closedClass: 'm-spoiler--closed',
+
+  duration: 500,
+  easing: 'easeInOutQuad',
+
+  closeSiblings: true,
+
+  openCb: () => {},
+  closeCb: () => {},
+};
+
+const toggleControlText = ($el, closed = false) => {
+  if ($el && $el.length) {
+    closed ? $el.text($el.data('openText')) : $el.text($el.data('closeText'));
+  }
+};
+
+export default (selector = '.js-spoiler', opts) => {
+  const settings = Object.assign({}, defaultSettings, opts);
+  const togglerSel = selector + settings.togglerSfx;
+  const controlSel = selector + settings.controlSfx;
+  const boxSel = selector + settings.boxSfx;
+
   $(selector).each((i, el) => {
     const $container = $(el);
-    const $toggler = $container.find(`${selector}-toggler`);
-    const $box = $container.find(`${selector}-box`);
-    const $content = $box.children().first();
-    let isCollapsed = $box.hasClass('-collapsed');
+    const $toggler = $container.find(togglerSel);
+    const $control = $container.find(controlSel);
+    const $box = $container.find(boxSel).css({ transform: 'translateZ(0)' });
 
-    const open = () => {
-      $toggler.removeClass('-collapsed');
-      $box.removeClass('-collapsed');
-      $box.css({ height: $content.outerHeight(true) });
-      isCollapsed = false;
+    let closed = $container.hasClass(settings.closedClass);
+    let inAnimation = false;
+
+    $box.css({
+      transform: 'translateZ(0)',
+      display: closed ? 'none' : 'block',
+    });
+
+    const completeCb = () => {
+      inAnimation = false;
+      closed = !closed;
+      closed ? settings.closeCb($container) : settings.openCb($container);
+      toggleControlText($control, closed);
     };
 
-    const close = () => {
-      $toggler.addClass('-collapsed');
-      $box.addClass('-collapsed');
-      $box.css({ height: 0 });
-      isCollapsed = true;
+    const animationOpts = {
+      duration: settings.duration,
+      easing: settings.easing,
+      complete: completeCb,
     };
 
-    $toggler.on('click', (e) => {
-      e.preventDefault();
-      if (isCollapsed) {
-        open();
+    const toggle = (e) => {
+      if (e) e.preventDefault();
+      if (inAnimation) return;
+      inAnimation = true;
+      $container.toggleClass(settings.closedClass);
+      if (closed) {
+        if (settings.closeSiblings) {
+          $container.siblings(selector).trigger('spoiler::close');
+        }
+        $box.velocity('slideDown', animationOpts);
       } else {
-        close();
+        $box.velocity('slideUp', animationOpts);
+      }
+    };
+
+    $toggler.on('click', toggle);
+    $control.on('click', toggle);
+
+    $container.on('spoiler::close', () => {
+      if (!closed) {
+        $box.velocity('slideUp', animationOpts);
+        $container.addClass(settings.closedClass);
+      }
+    });
+
+    $container.on('spoiler::open', () => {
+      if (closed) {
+        $box.velocity('slideDown', animationOpts);
+        $container.removeClass(settings.closedClass);
       }
     });
   });
