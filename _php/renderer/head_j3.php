2753a6641580a8dad07b8e5e92c745040122f8f8
Danila Buyanov <info@saity74.ru>
2017-04-12 11:55:47 +0500

    Update head_j3.php

--- _php/renderer/head_j3.php
+++ _php/renderer/head_j3.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @package     Joomla.Platform
- * @subpackage  Document
- *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE
- */
-
 defined('JPATH_PLATFORM') or die;
 
 use Joomla\Utilities\ArrayHelper;
